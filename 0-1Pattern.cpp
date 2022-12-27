8e8cf558afa9c1691c53d5845d5f434506310c11
Vikas Negi <negivikas201@gmail.com>
2022-12-27 17:53:55 +0530

    0-1Pattern

---
+++ 0-1Pattern.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+  {
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            if((i+j)%2==0)
+             {
+                cout<<"1";
+             }
+            else{
+                cout<<"0";
+            }
+        }   
+        cout<<endl;  
+    }   
+    return 0;
+  }
